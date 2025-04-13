b0e93a740e2b20cb041b9c650bb2b1f15dbe1ef4
BEN ABBOU Marouane <marouane.benabbou.2002@gmail.com>
2025-04-13 17:04:34 +0100

    prevous commit is the command creating this is is the actual model

--- price-tracker/backend/tracker/coreapi/models.py
+++ price-tracker/backend/tracker/coreapi/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Website(models.Model):
+    name = models.CharField(_("Website name"), max_length=50)
+
+
+class Product(models.Model):
+    class Category(models.TextChoices):
+        COMPONENTS = 'COMP', _('Components')
+        PERIPHERALS = 'PER', _('Peripherals')
+        
+    id = models.CharField(primary_key=True, max_length=50, unique=True)
+    name = models.CharField(_("Product name"), max_length=100, db_index=True)
+    short_description = models.TextField(_("Product Description"), blank=True, null=True)
+    url = models.URLField(_("Product URL"), max_length=200)
+    image_url = models.URLField(_("Product Image URL"), max_length=200)
+    price = models.DecimalField(_("Product Price"), max_digits=10, decimal_places=2)
+    availability = models.BooleanField(_("Product availability"), default=True)
+    category = models.CharField(_("Product Category"), max_length=4, choices=Category.choices)
+    website = models.ForeignKey(Website, verbose_name=_("Website"), on_delete=models.CASCADE, related_name="products")
+    created_at = models.DateTimeField(_("First scraped"), auto_now_add=True)
+    last_updated = models.DateTimeField(_("Last updated"), auto_now=True)
+    seen = models.BooleanField(default=False)
+
