ffebcf96e4805268b1a26f61eb30be374c7c24e6
BEN ABBOU Marouane <marouane.benabbou.2002@gmail.com>
2025-04-21 17:04:11 +0100

    created the view and added pagination

--- price-tracker/backend/tracker/coreapi/views.py
+++ price-tracker/backend/tracker/coreapi/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Product, Website
+from .serializers import ProductSerializer, WebsiteSerializer
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
