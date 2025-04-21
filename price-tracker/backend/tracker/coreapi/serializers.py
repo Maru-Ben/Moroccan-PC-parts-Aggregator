2bc37c618bd527396c6808a37edd36348ddd1fef
BEN ABBOU Marouane <marouane.benabbou.2002@gmail.com>
2025-04-21 17:03:48 +0100

    Update serializers.py

--- price-tracker/backend/tracker/coreapi/serializers.py
+++ price-tracker/backend/tracker/coreapi/serializers.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
-from models import Product, Website
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        exclude = ['seen', 'created_at', 'updated_at']
+from .models import Product, Website
 
 class WebsiteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Website
         exclude = ['created_at', 'updated_at']
+
+class ProductSerializer(serializers.ModelSerializer):
+    website = WebsiteSerializer()
+    category = serializers.CharField(source='get_category_display')
+    
+    class Meta:
+        model = Product
+        exclude = ['seen', 'created_at', 'updated_at']
