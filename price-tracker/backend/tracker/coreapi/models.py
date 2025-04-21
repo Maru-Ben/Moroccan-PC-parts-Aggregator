fbf3e57f00213ca4021587238e3be448850ffa17
BEN ABBOU Marouane <marouane.benabbou.2002@gmail.com>
2025-04-21 14:50:26 +0100

    renamed the updated_at and added also to the website model

--- price-tracker/backend/tracker/coreapi/models.py
+++ price-tracker/backend/tracker/coreapi/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 class Website(models.Model):
-    name = models.CharField(_("Website name"), max_length=50)
+    name = models.CharField(_("Website name"), max_length=50),
+    created_at = models.DateTimeField(_("First added"), auto_now_add=True),
+    updated_at = models.DateTimeField(_("Last updated"), auto_now=True)
 
 
 class Product(models.Model):
@@ -20,6 +22,6 @@ class Product(models.Model):
     category = models.CharField(_("Product Category"), max_length=4, choices=Category.choices)
     website = models.ForeignKey(Website, verbose_name=_("Website"), on_delete=models.CASCADE, related_name="products")
     created_at = models.DateTimeField(_("First scraped"), auto_now_add=True)
-    last_updated = models.DateTimeField(_("Last updated"), auto_now=True)
+    updated_at = models.DateTimeField(_("Last updated"), auto_now=True)
     seen = models.BooleanField(default=False)
 
