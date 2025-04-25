5cc453f8ed99cc06d2da655756dee44ba9e27d3e
BEN ABBOU Marouane <marouane.benabbou.2002@gmail.com>
2025-04-25 18:47:18 +0100

    added fuzzy search

--- price-tracker/backend/tracker/coreapi/views.py
+++ price-tracker/backend/tracker/coreapi/views.py
@@ -2,8 +2,24 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Product, Website
 from .serializers import ProductSerializer, WebsiteSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.contrib.postgres.search import TrigramSimilarity
+
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    
+
+    @action(detail=False, methods=['get'])
+    def search(self, request):
+        query = request.GET.get('query', '')
+        
+        if query:
+            # Fuzzy search
+            products = Product.objects.annotate(similarity = TrigramSimilarity('name', query)).filter(similarity__gte=0.1).order_by('-similarity')[:10]
+        else:
+            products = Product.objects.all()
+        
+        serializer = self.get_serializer(products, many=True)
+        return Response(serializer.data)
